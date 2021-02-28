df65a175c75c0d239d5d1f2d1281095c8b3adab6
Начаров Сергей <nacharovsa@it2g.ru>
2021-02-28 13:51:50 +0400

    feature: mapbox, preloader animation

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,52 +1,59 @@
 $(() => {
-  ymaps.ready(init);
-  var myMap;
-  var myPlacemark;
-  var centerPosition;
-
-  if ( $(window).width() < 768 ) {
-    centerPosition = [55.730138, 37.628312];
-  } else {
-    centerPosition = [55.729843, 37.632404]
-  }
-
-  function init(){
-    myMap = new ymaps.Map("map", {
-      center: centerPosition,
-      zoom: 16,
-      // scrollZoom: false,
-      controls: [],
-      disableDefaultUI: true
-    });
-    // myMap.behaviors.disable('drag');
-    myMap.behaviors.disable('scrollZoom');
-
-    myPlacemark = new ymaps.Placemark(
-        [55.730637, 37.628164],
-        {
-          balloonContent: '<div class="balloon__content"><div class="balloon__address"><span>VOS’HOD</span><span>Валовая ул., 26</span></div><div class="balloon__name">БЦ Lighthouse</div></div>',
+  var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          iconSize: [29,43]
         },
-        {
-          iconLayout: 'default#image',
-          iconImageHref: 'assets/img/balloon.svg',
-          iconImageSize: [29, 42],
-          iconImageOffset: [-14, -24],
-
-          balloonLayout: "default#imageWithContent",
-          balloonImageHref: 'assets/img/balloon.svg',
-          balloonImageOffset: [0, 0],
-          balloonImageSize: [29, 42],
-          balloonOffset: [-15, -25],
-          balloonShadow: false,
-        },
-        {
-          balloonCloseButton: false,
-          hideIconOnBalloonOpen: false,
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            37.62825965881348,
+            55.73074102854723
+          ]
         }
-    );
-
-    myMap.geoObjects.add(myPlacemark);
-    // console.log()
-    myPlacemark.balloon.open()
+      }
+    ]
   }
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoieGtvcmVkZXVzIiwiYSI6ImNrbGtxcmoybzNmMGIydnM4MWs5ZXo5cmMifQ._YIlgXTnQtUtNubeZg81dg';
+
+  if ( $(window).width() > 996 ) {
+    centerPosition = [
+      37.632057666778564,
+      55.730481255119734]
+  } else {
+    centerPosition = [
+      37.62949347496033,
+      55.73034230583267];
+  }
+
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/xkoredeus/cklkrgmef0qsx17mlzyqyo3ga',
+    center: centerPosition,
+    zoom: 16,
+    attributionControl: false
+  });
+
+  // add markers to map
+  geojson.features.forEach(function (marker) {
+    // create a DOM element for the marker
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage =
+        'url(assets/img/balloon.svg)';
+    el.style.width = marker.properties.iconSize[0] + 'px';
+    el.style.height = marker.properties.iconSize[1] + 'px';
+
+    // add marker to map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+  });
+
+  var elInner = $('.balloon__content');
+  $('.marker').append(elInner);
 });
